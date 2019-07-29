dde6ce12f86a8df383a399b54a3244e072cc25b8
egnaf <mamadaliev@yahoo.com>
2019-07-29 13:37:09 +0300

    Add Encrypt class

---
+++ src/main/java/org/perfectstorms/life4j/utils/Encrypt.java
@@ -0,0 +1,14 @@
+package org.perfectstorms.life4j.utils;
+
+import org.perfectstorms.life4j.Cell;
+
+public class Encrypt {
+
+    public static String encode(Cell[][][] cells) {
+        return "";
+    }
+
+    public static Cell[][][] decode(String data) {
+        return new Cell[0][][];
+    }
+}
